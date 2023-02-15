f321409dec64671811a42831a9757e93aa1d824a
Dasun47 <dasunlakshan47@gmail.com>
2023-02-15 22:39:59 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./App.css";
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Books",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e3",
+    title: "Giutar",
+    amount: 200.99,
+    date: new Date(2021, 4, 12),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 1),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+  //const [title, setTitle] = useState(props.title);
+  //console.log("ExpenseItem evaluated by React");
+
+  // const clickHandler = () => {
+  //   setTitle("Updated!");
+  //   console.log(title);
+  // };
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ExpenseItem;
